3feff98c13d3f5f68a59ca72c1108ec097e309dc
faride30 <faride.chak@gmx.de>
2016-12-13 10:14:02 +0100

    first commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+
+package helloworld;
+
+/**
+ *
+ * @author chak.faride
+ */
+public class HelloWorld {
+
+    
+    public static void main(String[] args) {
+      System.out.println("Hello World!");
+    }
+    
+}
+    
+    
+
